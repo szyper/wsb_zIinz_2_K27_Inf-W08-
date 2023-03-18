d8bb79c93ee125e7753953ff7d3699d89cb750f1
szyper <zskgit@gmail.com>
2023-03-18 12:40:41 +0100

    php db

---
+++ 3_db/2_db_table.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Użytkownicy</title>
+</head>
+<body>
+<h4>Użytkownicy</h4>
+<?php
+	require_once "../scripts/connect.php";
+?>
+</body>
+</html>
--- scripts/connect.php
+++ scripts/connect.php
@@ -1,4 +1,4 @@
 <?php
   $conn = new mysqli("localhost", "root", "", "wsb_ziinz_2_k27_inf (w08)");
-  // echo "db";
- ?>
+  echo "db";
+	echo $conn->connect_errno; //0 -> ok
