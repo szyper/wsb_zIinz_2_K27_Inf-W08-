a5c23f2a7a067fa126cb66fe8ba0bf2dff92e65f
szyper <zskgit@gmail.com>
2023-04-01 11:45:04 +0200

    php

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,4 +1,4 @@
 <?php
-  $conn = new mysqli("localhost", "wsb", "XeU7UiN7cZ0Uf4kg", "wsb_ziinz_2_k27_inf (w08)");
+  $conn = new mysqli("localhost", "root", "", "wsb_ziinz_2_k27_inf (w08)");
   //echo "db";
 	//echo $conn->connect_errno; //0 -> ok
